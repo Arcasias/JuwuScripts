1dbaa0b554452a4b9ed8552db1a64d651c8857f6
Julien Mougenot <julmougenot@gmail.com>
2022-09-29 19:51:30 +0200

    improve storage cleaner

--- src/public/storage_cleaner.js
+++ src/public/storage_cleaner.js
@@ -4,8 +4,10 @@
  * @description Clean local and session storages and prevent them from being used.
  */
 
-Storage.prototype.setItem = (key) => {
-  console.debug(`Storage: prevented "${key}" from being added.`);
+Storage.prototype.setItem = (key, value) => {
+  console.debug(
+    `Storage: prevented 'setItem' with arguments key="${key}" and value="${value}".`
+  );
 };
 for (const storage of [window.localStorage, window.sessionStorage]) {
   storage.clear();
